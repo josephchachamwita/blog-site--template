5388ecc034fc3f7ee4346fd6dfc6dc3370f19825
joseph chacha mwita <chachajoseph845@gmail.com>
2025-12-06 15:54:03 +0300

    changed my backend default port to 5000

--- Backend/server.js
+++ Backend/server.js
@@ -28,6 +28,8 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://blog-site-template-pi.vercel.app",
+  " https://blog-site-template-1.onrender.com"
+
 ];
 
 app.use(
@@ -289,5 +291,5 @@ app.delete("/deletepost/:id", verifyUser, async (req, res) => {
 /* =====================================================
    START SERVER
 ===================================================== */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
